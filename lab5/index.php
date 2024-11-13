b4b0c27c9400b88f7e085c31296627b2c2d56233
CytroxCoder <115219628+KamilDawidISI1@users.noreply.github.com>
2024-11-13 17:16:10 +0100

    Update index.php

--- lab5/index.php
+++ lab5/index.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
 if($_GET['idp'] =='') $strona = 'html/glowna.html';
 if($_GET['idp']=='Dama') $strona = 'html/Dama.html';
 if($_GET['idp']=='DTP') $strona = 'html/DTP.html';
-if($_GET['idp']=='Form') $strona = 'html/From.html';
+if($_GET['idp']=='Form') $strona = 'html/Form.html';
 if($_GET['idp']=='Krym') $strona = 'html/Krym.html';
 if($_GET['idp']=='NWP') $strona = 'html/NWP.html';
 if($_GET['idp']=='Slow') $strona = 'html/Slow.html';
