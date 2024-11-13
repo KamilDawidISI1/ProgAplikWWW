2251a01aeaa54906388967e1886c8ea084d7dc78
CytroxCoder <115219628+KamilDawidISI1@users.noreply.github.com>
2024-11-13 17:14:06 +0100

    Add files via upload

--- lab5/index.php
+++ lab5/index.php
@@ -1,55 +1,32 @@
 <?php
 error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
-if ($_GET['idp'] == '') {
-    $strona = 'html/glowna.html';
-} elseif ($_GET['idp'] == 'Dama') {
-    $strona = 'html/Dama.html';
-} elseif ($_GET['idp'] == 'DTP') {
-    $strona = 'html/DTP.html';
-} elseif ($_GET['idp'] == 'Krym') {
-    $strona = 'html/Krym.html';
-} elseif ($_GET['idp'] == 'NWP') {
-    $strona = 'html/NWP.html';
-} elseif ($_GET['idp'] == 'Slow') {
-    $strona = 'html/Slow.html';
-} else {
-    $strona = 'html/glowna.html';
-}
 
-if (file_exists($strona)) {
-    include($strona);
-} else {
-    echo "Nie znaleziono strony.";
-}
+
+if($_GET['idp'] =='') $strona = 'html/glowna.html';
+if($_GET['idp']=='Dama') $strona = 'html/Dama.html';
+if($_GET['idp']=='DTP') $strona = 'html/DTP.html';
+if($_GET['idp']=='Form') $strona = 'html/From.html';
+if($_GET['idp']=='Krym') $strona = 'html/Krym.html';
+if($_GET['idp']=='NWP') $strona = 'html/NWP.html';
+if($_GET['idp']=='Slow') $strona = 'html/Slow.html';
 
 ?>
-
-
-
-
-
 <!DOCTYPE html>
 <html lang="pl">
 <head>
-  <meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
-  <meta http-equiv="Content-Language" content="pl" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="Author" content="Kamil Dawid" />
-  <title>Moje hobby to podcasty</title>
-  <link rel="stylesheet" href="css/style.css">
-  <script src="js/kolorujtlo.js" type="text/javascript"></script>
-  <script src="js/timedate.js" type="text/javascript"></script>
-  <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-  <script>
-    $(document).ready(function () {
-      $("header").click(function (){
-        $(this).toggleClass("header_animation")
-      });
-    });
-  </script>
-
+    <meta http-equiv="Content-type" content="text/html; charset=UTF-8"/>
+    <meta http-equiv="Content-Language" content="pl"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Author" content="Kamil Dawid"/>
 </head>
 <body onload="startClock()">
-
+<?php
+    if (file_exists($strona)) {
+        include($strona);
+    } else
+        echo 'Nie działa'
+?>
 </body>
 </html>
+
+
